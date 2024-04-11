8f2779f3ff7efddb83176c609230842a1d275b80
Mathias <599028@stud.hvl.no>
2024-04-11 15:57:56 +0200

    skal nå fungere med n2k insight

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@
 *********/
 #include <Arduino.h>
 #include <Wire.h>
+#include "BluetoothSerial.h"
 
-#define I2C_DEV_ADDR 0x42
+#define I2C_DEV_ADDR 0x08
+
+BluetoothSerial SerialBT;
 
 int count = 0;
 
@@ -29,12 +32,34 @@ void setup()
 {
   Wire.setPins(23, 22);
   Wire.begin();
+
   Serial.begin(115200);
+  SerialBT.begin();
+
   Serial.println("\nIIC Master");
+  SerialBT.println("\nIIC Master (BT)");
+
+  
 }
 
 void loop()
 {
+  if (SerialBT.available())
+  {
+    byte inputByte = (byte)SerialBT.read();
+
+    Wire.beginTransmission(I2C_DEV_ADDR);
+    Wire.write(inputByte);
+    Wire.endTransmission();
+
+    Wire.requestFrom(I2C_DEV_ADDR, 1);
+    while (Wire.available())
+    {
+      byte b1 = Wire.read();
+      // byte b2=Wire.read();
+      SerialBT.printf("\n" + b1);
+    }
+  }
   if (count <8) {
     Serial.printf("%u\n",count);
     requestByte(count);
