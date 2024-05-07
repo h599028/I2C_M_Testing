e1a87d58db0c72e3c754decd87d8bc1f4d911203
Mathias Madsen Aaøyen <599028@stud.hvl.no>
2024-05-07 13:59:58 +0200

    hmm

--- src/main.cpp
+++ src/main.cpp
@@ -11,66 +11,47 @@
 
 
 
-BluetoothSerial SerialBT;
-byte pgn_request= (byte) I2C_PGN_REQ;
+byte pgn_request = (byte)I2C_PGN_REQ;
 int count = 0;
-
-void requestByte(byte myIndex)
-{
-  Wire.beginTransmission(I2C_DEV_ADDR);
-  Wire.write(myIndex);
-  Wire.endTransmission();
-
-  Wire.requestFrom(I2C_DEV_ADDR, 1);
-  while (Wire.available())
-  {
-    byte b1 = Wire.read();
-    // byte b2=Wire.read();
-    Serial.printf("%u", b1);
-  }
-
-}
 
 void setup()
 {
-  Wire.setPins(23, 22);
-  Wire.begin();
+	Wire.setPins(23, 22);
+	Wire.begin();
 
-  Serial.begin(115200);
-  SerialBT.begin();
-
-  Serial.println("\nIIC Master");
-  SerialBT.println("\nIIC Master (BT)");
-
-  
+	Serial.begin(115200);
+	Serial.println("\nIIC Master");
 }
 
 void loop()
 {
-  if (SerialBT.available())
-  {
-    byte inputByte[] = {pgn_request, 0x01, 0xf8, 0x02};
-    byte length;
-    Wire.beginTransmission(I2C_DEV_ADDR);
-    Wire.write(inputByte, 4);
-    Wire.endTransmission();
 
-    Wire.requestFrom(I2C_DEV_ADDR, 1);
-    while (Wire.available())
-    {
-      length = Wire.read();
-      // byte b2 = Wire.read();
-      SerialBT.printf("\n%d", length);
-    }
-    Wire.requestFrom(I2C_DEV_ADDR, length);
-    while (Wire.available()) 
-    {
-      while (length >0) 
-      {
-        
-      }
-    }
-  }
+	Serial.println("\nIIC Master");
 
-  delay(5000);
+	byte inputByte[] = {0x08, 0x01, 0xf8, 0x02};
+	int length;
+	Wire.beginTransmission(I2C_DEV_ADDR);
+	Wire.write(inputByte, 4);
+	Wire.endTransmission();
+
+	Wire.requestFrom(I2C_DEV_ADDR, 2);
+	while (Wire.available())
+	{
+		int b1 = (int) Wire.read();
+		int b2 = (int) Wire.read();
+
+ 		length = (b1 << 8) | b2;
+
+		Serial.printf("Length : %d ", length );
+		
+	}
+
+	Wire.requestFrom(I2C_DEV_ADDR, length);
+	while (Wire.available()) {
+		
+		Serial.printf("%c", (char)Wire.read());
+		
+	}
+
+	delay(5000);
 }
